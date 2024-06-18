9698e124ebe117bb1ae5f6534a37846e6042a17f
sarahairunnisa <sarah.kp1005@gmail.com>
2024-06-19 01:03:40 +0700

    update-landing-page

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,8 +1,8 @@
-<footer class="bg-white text-black py-4 shadow-md">
+<footer class="bg-white text-black py-4 shadow-md font-['Quicksand']">
     <div class="container ml-5">
         <div class="flex justify-between ">
             <div>
-               <ul class="space-y-5">
+               <ul class="space-y-3">
                 <li><a class="font-bold ">Contact Us</a></li>
                 <li><a class=""><a class="">customer.service@sayangdibuang.com</a></li>
                 <li><a class=""><a class="">Tel: +62 812 123 456 78</a></li>
@@ -13,16 +13,16 @@
             <div>
                 <a class="font-bold">Follow US</a>
                 <div class="flex space-x-11 mt-2">
-                    <a href=""><img class="w-11 h-11" src="img/logo-insta.png" alt="">
-                    <a href=""><img class="w-11 h-11" src="img/logo-x.png" alt="">
-                    <a href=""><img class="w-11 h-11" src="img/logo-tiktok.png" alt="">
+                    <a href=""><img class="w-6 h-6" src="img/logo-insta.png" alt="">
+                    <a href=""><img class="w-6 h-6" src="img/logo-x.png" alt="">
+                    <a href=""><img class="w-6 h-6" src="img/logo-tiktok.png" alt="">
                 </div>
             </div>
         </div>
-        <hr class="border-t border-gray-600 my-5">
+        <hr class="border-t border-gray-600">
     </div>
     <div class="container ml-5">
         <div class="justify-between mt-5 ">
-            <a href="">Copyright 2024</a>
+            <a href="">© Copyright 2024</a>
         </div>
 </footer>
